696671a45b5458327561c3a5c548aa949457caf4
Alessio Onori <alessio.onori@gmail.com>
2018-03-24 16:06:44 +0100

    real commit

--- Join/real.h
+++ Join/real.h
@@ -2,13 +2,17 @@
  * real.h
  *
  *  Created on: 24/mar/2018
- *      Author: Dario
+ *      Author: Dario, Alessio
  */
 
 #ifndef REAL_H_
 #define REAL_H_
 
 typedef double real;
+/*
+ * utilizziamo REAL_MULT per rendere omogenei i casi
+ * con i fixed point
+ */
 #define REAL_MULT(R1,R2) ((R1)*(R2))
 
 #endif /* REAL_H_ */
